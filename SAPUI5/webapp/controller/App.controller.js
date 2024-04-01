8c0bcf0652183e2799adce13647614421693d526
FioriSAP <fiorisap007@gmail.com>
2024-04-01 20:56:20 +0000

    5.6. Módulos

--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -1,20 +1,22 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/m/MessageToast"
     ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.m.MessageToast} MessageToast 
      * @returns 
      */
-    function(Controller) {
+    function(Controller, MessageToast) {
       "use strict";
   
       return Controller.extend("nsdprb.SAPUI5.controller.App", {
         onInit: function() {
         },
         onShowHello:function(){
-            alert("Hola Mundo");
+          MessageToast.show("Hola Mundo");
         }
       });
     }
