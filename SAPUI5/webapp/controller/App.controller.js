fb57964a3589759e6aeaa59f8e2aa6ca7d53a4d5
FioriSAP <fiorisap007@gmail.com>
2024-04-01 23:06:37 +0000

    5.9. Internacionalización - i18n

--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -2,23 +2,34 @@ sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
         "sap/m/MessageToast",
-        "nsdprb/SAPUI5/model/Models"
+        "nsdprb/SAPUI5/model/Models",
+        "sap/ui/model/resource/ResourceModel"
     ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      * @returns 
      */
-    function(Controller, MessageToast, Models) {
+    function(Controller, MessageToast, Models,ResourceModel) {
       "use strict";
   
       return Controller.extend("nsdprb.SAPUI5.controller.App", {
         onInit: function() {
+          //Set data model on  the view
           this.getView().setModel(Models.createRecipient());
+
+          //Set I18N model on the view
+          const i18nModel = new ResourceModel({bundleName:"nsdprb.SAPUI5.i18n.i18n"});
+          this.getView().setModel(i18nModel,"i18n");
         },
         onShowHello:function(){
-          MessageToast.show("Hola Mundo");
+          //Read text frp, i18n model
+          const oBundle = this.getView().getModel("i18n").getResourceBundle();
+          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+          const sMsg = oBundle.getText("helloMsg",[sRecipient]);
+          MessageToast.show(sMsg);
         }
       });
     }
