450f89df9408fa56508f63975087380609f84a10
FioriSAP <fiorisap007@gmail.com>
2024-04-01 21:16:16 +0000

    5.7. Modelo JSON

--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -1,19 +1,24 @@
 sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast"
+        "sap/m/MessageToast",
+        "sap/ui/model/json/JSONModel"
     ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
      * @returns 
      */
-    function(Controller, MessageToast) {
+    function(Controller, MessageToast, JSONModel) {
       "use strict";
   
       return Controller.extend("nsdprb.SAPUI5.controller.App", {
         onInit: function() {
+          const oData = { recipient : { name:"World"} };
+          const oModel = new JSONModel(oData);
+          this.getView().setModel(oModel);
         },
         onShowHello:function(){
           MessageToast.show("Hola Mundo");
