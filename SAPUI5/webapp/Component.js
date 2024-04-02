0ea22bf2131025f4ca49e9e1e9a1e04c4bdd0397
FioriSAP <fiorisap007@gmail.com>
2024-04-02 12:35:37 +0000

    5.10. Componentes - Configuración

---
+++ SAPUI5/webapp/Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "nsdprb/SAPUI5/model/Models",
+    "sap/ui/model/resource/ResourceModel"
+],
+/**
+ * 
+ * @param {typeof sap.ui.core.UIComponent} UIComponent 
+ * @returns 
+ */
+function (UIComponent,Models,ResourceModel) {
+    "use strict";
+
+    return UIComponent.extend("nsdprb.SAPUI5.Component", {
+
+        init: function () {
+            // call the base component's init function
+            UIComponent.prototype.init.apply(this, arguments);
+
+            //Set data model on  the view
+            this.setModel(Models.createRecipient());
+
+            //Set I18N model on the view
+            const i18nModel = new ResourceModel({bundleName:"nsdprb.SAPUI5.i18n.i18n"});
+            this.setModel(i18nModel,"i18n");
+
+        }
+    });
+}
+);
--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -1,28 +1,20 @@
 sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast",
-        "nsdprb/SAPUI5/model/Models",
-        "sap/ui/model/resource/ResourceModel"
+        "sap/m/MessageToast"
     ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast
-     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
      * @returns 
      */
-    function(Controller, MessageToast, Models,ResourceModel) {
+    function(Controller, MessageToast) {
       "use strict";
   
       return Controller.extend("nsdprb.SAPUI5.controller.App", {
         onInit: function() {
-          //Set data model on  the view
-          this.getView().setModel(Models.createRecipient());
 
-          //Set I18N model on the view
-          const i18nModel = new ResourceModel({bundleName:"nsdprb.SAPUI5.i18n.i18n"});
-          this.getView().setModel(i18nModel,"i18n");
         },
         onShowHello:function(){
           //Read text frp, i18n model
