0ce2697c296f542bea0b729aff922d98ad16ec9c
FioriSAP <fiorisap007@gmail.com>
2024-04-02 19:23:21 +0000

    7.3. Dialog Definicion

--- SAPUI5/webapp/controller/HelloPanel.controller.js
+++ SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -1,15 +1,17 @@
 sap.ui.define(
   [
       "sap/ui/core/mvc/Controller",
-      "sap/m/MessageToast"
+      "sap/m/MessageToast",
+      "sap/ui/core/Fragment"
   ],
   /**
    * 
    * @param {typeof sap.ui.core.mvc.Controller} Controller 
    * @param {typeof sap.m.MessageToast} MessageToast
+   * @param {typeof sap.ui.core.Fragment} Fragment
    * @returns 
    */
-  function(Controller, MessageToast) {
+  function(Controller, MessageToast, Fragment ) {
     "use strict";
 
     return Controller.extend("nsdprb.SAPUI5.controller.HelloPanel", {
@@ -22,6 +24,20 @@ sap.ui.define(
         const sRecipient = this.getView().getModel().getProperty("/recipient/name");
         const sMsg = oBundle.getText("helloMsg",[sRecipient]);
         MessageToast.show(sMsg);
+      },
+      onOpenDialog:function(){
+        const oView = this.getView();
+        if(!this.byId("helloDialog")){
+          Fragment.load({
+            id: oView.getId(),
+            name:"nsdprb.SAPUI5.view.HelloDialog"
+          }).then(function (oDialog){
+            oView.addDependent(oDialog);
+            oDialog.open();
+          });
+        }else{
+          this.byid("helloDialog").open();
+        }
       }
     });
   }
