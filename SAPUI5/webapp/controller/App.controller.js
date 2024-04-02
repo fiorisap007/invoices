027a406699e53cc2c2a7bcbba563f81f38ec537b
FioriSAP <fiorisap007@gmail.com>
2024-04-02 16:25:23 +0000

    7.1. Vistas anidadas

--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -1,27 +1,18 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast"
+        "sap/ui/core/mvc/Controller"
     ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     * @param {typeof sap.m.MessageToast} MessageToast
      * @returns 
      */
-    function(Controller, MessageToast) {
+    function(Controller) {
       "use strict";
   
       return Controller.extend("nsdprb.SAPUI5.controller.App", {
         onInit: function() {
 
-        },
-        onShowHello:function(){
-          //Read text frp, i18n model
-          const oBundle = this.getView().getModel("i18n").getResourceBundle();
-          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          const sMsg = oBundle.getText("helloMsg",[sRecipient]);
-          MessageToast.show(sMsg);
         }
       });
     }
